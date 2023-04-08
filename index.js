286040fbdf84bd1f1252d8ccb7be744aee4ee108
Samuel Victor <okorosamuelokafor@gmail.com>
2023-04-08 03:17:44 +0100

    simple calculator

---
+++ index.js
@@ -0,0 +1,169 @@
+"use strict"; //strict mode dissallows the use of undecleared var
+
+var input = document.getElementById('input'), // input/output button
+    number = document.querySelectorAll('.numbers div'), // number buttons
+    operator = document.querySelectorAll('.operators div'), // operator buttons
+    result = document.getElementById('result'), // equal button
+    clear = document.getElementById('clear'), // clear button
+    resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+// adding click handlers to number buttons
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function (e) {
+
+        // storing current input string and its last character in variables - used later
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
+
+        // if result is not diplayed, just keep adding
+        if (resultDisplayed === false) {
+            input.innerHTML += e.target.innerHTML;
+        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            // if result is currently displayed and user pressed an operator
+            // we need to keep on adding to the string for next operation
+            resultDisplayed = false;
+            input.innerHTML += e.target.innerHTML;
+        } else {
+            // if result is currently displayed and user pressed a number
+            // we need clear the input string and add the new input to start the new opration
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+// adding click handlers to number buttons
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function (e) {
+
+        // storing current input string and its last character in variables - used later
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
+
+        // if last character entered is an operator, replace it with the currently pressed one
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+            input.innerHTML = newString;
+        } else if (currentString.length == 0) {
+            // if first key pressed is an opearator, don't do anything
+            console.log("enter a number first");
+        } else {
+            // else just add the operator pressed to the input
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+// on click of 'equal' button
+result.addEventListener("click", function () {
+
+    // this is the string that we will be processing eg. -10+26+33-56*34/23
+    var inputString = input.innerHTML;
+
+    // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+    //
+    var numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+    // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+    // first we replace all the numbers and dot with empty string and then split
+    //regular expression allows u to search through a string of text bind it so u can use to do several things
+    var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+
+    // now we are looping through the array and doing one operation at a time.
+    // first divide, then multiply, then subtraction and then addition
+    // as we move we are alterning the original numbers and operators array
+    // the final element remaining in the array will be the output
+
+    var divide = operators.indexOf("÷"); //search for the string of division
+    while (divide != -1) {
+        //replaces the  numbers[divide] / numbers[divide + 1] by the result 
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    var multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+
+    var subtract = operators.indexOf("-"); //search for d string of -
+    while (subtract != -1) {  //while d s
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+
+    var add = operators.indexOf("+"); //search for the string of +
+    while (add != -1) {
+        // using parseFloat is necessary, otherwise it will result in string concatenation :)
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0] // displaying the output
+
+    resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener ("click", ()=>  input.innerHTML = "")
+
+
+
+
+
+
+
+
+
+
+
+//selectors
+let screen = document.querySelector('.screen');
+let panel = document.querySelector('.panel');
+let equal = document.getElementById('equal')
+
+//eventListeners
+panel.addEventListener('click', numberToScreen);
+equal.addEventListener('click', resolve)
+
+//functions
+// if we push a button on calculator
+function numberToScreen(num){
+  
+  //if its C, delete last digit
+   const button = num.target.innerHTML;
+  if(button==='C'){
+   let onScreen = (screen.innerHTML)
+   screen.innerHTML = onScreen.slice(0, -1)
+    
+   //else if its 0 on screen, replace it with the pushed buttons digit
+  }else
+  if(screen.innerHTML=== '0'){
+    screen.innerHTML = button
+    
+    //else, add the pushed buttons digit to the end 
+  }else{
+    screen.innerHTML += button
+  }
+ }
+
+function resolve(){
+  
+  //what is written on screen
+  let onScreen = (screen.innerHTML)
+  
+// resolve what is on screen,  to the nearest thousandth
+  screen.innerHTML=Math.round(eval(onScreen) * 1000) / 1000
+}
